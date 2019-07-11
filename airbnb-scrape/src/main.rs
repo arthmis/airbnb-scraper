09fe27032fb47ec769740207a74197ff51877d70
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-07-11 15:40:24 -0400

    Can now retrieve the page source after getting to the list of homes after filtering

--- airbnb-scrape/src/main.rs
+++ airbnb-scrape/src/main.rs
@@ -15,72 +15,41 @@ fn main() {
     let mut geckodriver = Command::new("geckodriver")
         .spawn()
         .expect("geckodriver failed to start");
-    println!("{}", geckodriver.id());
     let client = Client::new("http://localhost:4444");
-    // let location = "San Francisco, CA";
+    let location = "San Francisco, CA";
 
-    // tokio::run(
-    //     client
-    //         .map_err(|error| panic!("failed to connect to WebDriver: {:?}", error))
-    //         .and_then(|client| {
-    //             client.goto("https://www.google.com")
-    //         })
-    //         .and_then(|mut client| {
-    //             client.form(Locator::Id("tsf"))
-    //         })
-    //         .and_then(|mut form| {
-    //             form.set(
-    //                 Locator::XPath(r#"//input[@class="gLFyf gsfi" and @name="q" and @type="text"]"#),
-    //                 "rust"
-    //             )
-    //         })
-    //         .and_then(|mut form| {
-    //             // form.submit()
-    //             form.submit_direct()
-    //         })
-    //         .and_then(|mut client| {
-    //             client.persist()
-    //         })
-    //         .map_err(|error| {
-    //             panic!("a WebDriver command failed: {:?}", error);
-    //         })
-    //
-    // );
     let start_date_aria_label = create_aria_label(12, "July", 2019, "Friday", Date::Begin);
     let end_date_aria_label = create_aria_label(15, "July", 2019, "Monday", Date::End);
 
-    tokio::run(
+    let scrape_airbnb = 
         client
             .map_err(|error| unimplemented!("failed to connect to WebDriver: {:?}", error))
             .and_then(|client| client.goto("https://www.airbnb.com"))
-            // .and_then(|mut _client| {
-            //     Delay::new(when)
-            // })
-            .and_then(move |client| delay(client, 10_000))
+            .and_then(|client| delay(client, 5000))
             .and_then(|mut client| {
                 client.form(Locator::Id("MagicCarpetSearchBar"))
             })
-            .and_then(|mut form| {
+            .and_then(move |mut form| {
                 form.set(
                     Locator::Id("Koan-magic-carpet-koan-search-bar__input"),
-                    "San Francisco, CA",
+                    location,
                 )
             })
             .and_then(|form| form.submit_sneaky("type", "submit"))
-            .and_then(move |client| delay(client, 5000))
+            .and_then(|client| delay(client, 4000))
             .and_then(|client| {
                 client.wait_for_find(Locator::XPath(
                     r#"//button[@aria-controls="menuItemComponent-date_picker"]"#,
                 ))
             })
             .and_then(|element| element.click())
-            .and_then(move |client| {
-                delay(client, 4000)
+            .and_then(|client| {
+                delay(client, 2000)
             })
             .and_then(move |client| client.wait_for_find(Locator::XPath(&start_date_aria_label)))
             .and_then(|element| element.click())
-            .and_then(move |client| {
-                delay(client, 3000)
+            .and_then(|client| {
+                delay(client, 2000)
             })
             .and_then(move |client| client.wait_for_find(Locator::XPath(&end_date_aria_label)))
             .and_then(|element| element.click())
@@ -90,19 +59,28 @@ fn main() {
                 ))
             })
             .and_then(|element| element.click())
-            .and_then(move |client| {
-                delay(client, 4000)
+            .and_then(|client| {
+                delay(client, 2000)
             })
             .and_then(|client| {
                 client.wait_for_find(Locator::XPath(r#"//img[@alt="Entire homes"]"#))
             })
             .and_then(|element| element.click())
-            .and_then(|mut client| client.persist())
+            .and_then(|client| {
+                delay(client, 4000)
+            })
+            .and_then(|mut client| {
+                client.source()
+            })
+            // .and_then(|mut client| client.persist())
             .map_err(|error| {
                 panic!("a WebDriver command failed: {:?}", error);
-            })
-            .map(|_| ()),
-    );
+            });
+
+    // need to use this with `runtime.block_on() to return the result of an executed future`
+    let mut runtime = tokio::runtime::Runtime::new().expect("Unable to start runtime");
+    let page_source = runtime.block_on(scrape_airbnb).expect("page source not found");
+    println!("source: {:?}", page_source);
     geckodriver.wait().expect("Command wasn't running");
 }
 
@@ -118,11 +96,7 @@ fn create_aria_label(day: u8, month: &str, year: u16, day_name: &str, date: Date
 }
 
 fn delay(client: Client, time: u64) -> impl Future<Item = Client, Error = CmdError> {
-    Delay::new(delay_time(time))
+    Delay::new(Instant::now() + Duration::from_millis(time))
         .and_then(|_| Ok(client))
         .map_err(|error| panic!("delay errored; err: {:?}", error))
-}
-
-fn delay_time(time: u64) -> Instant {
-    Instant::now() + Duration::from_millis(time)
 }
